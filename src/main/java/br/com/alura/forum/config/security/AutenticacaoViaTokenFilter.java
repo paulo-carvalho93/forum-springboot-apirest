3fce3d52c090f1413e30ce04eb197e8ee8f4fcb8
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-05-15 19:40:41 -0300

    Autenticando o cliente com Spring Security

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -7,15 +7,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum.model.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 	
 	private TokenService tokenService;
+	private UsuarioRepository usuarioRepository;
 	
-	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
 		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
 	}
 
 	@Override
@@ -25,8 +31,18 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 		String token = recuperarToken(request);
 		boolean valido = tokenService.isTokenValido(token);
 		
+		if (valido) {
+			autenticarCliente(token);
+		}
 		
 		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
@@ -38,5 +54,4 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 		
 		return token.substring(7, token.length());
 	}
-
 }
